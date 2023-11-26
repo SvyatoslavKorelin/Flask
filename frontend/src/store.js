85de15b9fe9fe6d0d6cb4a8c98b92fb605fb01a1
GoldenEagle74 <mshchannikov@yandex.ru>
2023-11-26 19:59:06 +0300

    added store logic

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,21 +1,41 @@
-// // frontend/src/store.js
+import { createStore } from 'vuex';
 
-// import Vue from 'vue'
-// import Vuex from 'vuex'
+const store = createStore({
+  state: {
+    products: [], // Initial empty array, you can replace it with your data
+  },
+  mutations: {
+    setProducts(state, products) {
+      state.products = products;
+    },
+    addProduct(state, product) {
+      state.products.push(product);
+    },
+    // Add more mutations as needed
+  },
+  actions: {
+    fetchProducts({ commit }) {
+      // Replace this with your actual API call to fetch products
+      const fakeApiData = [
+        { id: 1, name: 'Product 1', price: 50 },
+        { id: 2, name: 'Product 2', price: 75 },
+        // Add more products as needed
+      ];
+      commit('setProducts', fakeApiData);
+    },
+    addNewProduct({ commit }, product) {
+      // Replace this with your actual API call to add a new product
+      // After adding the product, commit 'addProduct' mutation
+      commit('addProduct', product);
+    },
+    // Add more actions as needed
+  },
+  getters: {
+    getProductById: (state) => (id) => {
+      return state.products.find((product) => product.id === id);
+    },
+    // Add more getters as needed
+  },
+});
 
-// Vue.use(Vuex)
-
-// export default new Vuex.Store({
-//   state: {
-//     // Your state variables go here
-//   },
-//   mutations: {
-//     // Your mutations go here
-//   },
-//   actions: {
-//     // Your actions go here
-//   },
-//   modules: {
-//     // Your modules go here
-//   }
-// })
+export default store;
